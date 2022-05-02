6c296416ef34c65f5e70b889a29e96afc8a97d63
AymenWlf <alinzgohi@gmail.com>
2022-05-02 14:21:46 +0000

    Description of prices

--- assets/components/pricing.vue
+++ assets/components/pricing.vue
@@ -16,20 +16,21 @@
           <div class="box featured">
             <h3>Site Vitrine</h3>
             <h4><span>A partir de </span>2990<sup>Dh</sup></h4>
-            <div class="btn-wrap">
-              <a href="#" class="btn-buy">Demander un devis</a>
-            </div>
-          </div>
-        </div>
-
-        <div
-          class="col-lg-4 col-md-6 mt-4 mt-md-0"
-          data-aos="fade-up"
-          data-aos-delay="100"
-        >
-          <div class="box">
-            <h3>Site E-Commerce</h3>
-            <h4><span>A partir de </span>4990<sup>Dh</sup></h4>
+            <p class="text-justify">
+              Un site vitrine, est un site web qui présente votre entreprise,
+              organisation ou même vos produits
+            </p>
+            <hr />
+            <ul>
+              <span>
+                <li>SITE RAPIDE</li>
+                <li>MAINTENANCE ET ASSISTANCE</li>
+                <li class="color-orange">LOGO ET DEPLOIEMENT GRATUIT</li>
+                <li class="color-orange">
+                  ADAPTATEE POUR PC, TABLETTE ET TELEPHONE
+                </li>
+              </span>
+            </ul>
             <div class="btn-wrap">
               <a href="#" class="btn-buy">Demander un devis</a>
             </div>
@@ -44,7 +45,56 @@
           <div class="box">
             <span class="advanced">Avancée</span>
             <h3>Application WEB</h3>
+            <h4><span>A partir de </span>4990<sup>Dh</sup></h4>
+            <p class="text-justify">
+              Une application WEB de gestion, est un logiciel en ligne que vous
+              pouvez y accéder grace à votre navigateur internet pour gérer vos
+              clients, factures, fournisseurs... depuis votre PC, Tablette ou
+              même téléphone portable
+            </p>
+            <hr />
+            <ul>
+              <span>
+                <li>SITE RAPIDE</li>
+                <li>MAINTENANCE ET ASSISTANCE</li>
+                <li class="color-orange">LOGO ET DEPLOIEMENT GRATUIT</li>
+                <li class="color-orange">
+                  ADAPTATEE POUR PC, TABLETTE ET TELEPHONE
+                </li>
+              </span>
+            </ul>
+            <div class="btn-wrap">
+              <a href="#" class="btn-buy">Demander un devis</a>
+            </div>
+          </div>
+        </div>
+
+        <div
+          class="col-lg-4 col-md-6 mt-4 mt-md-0"
+          data-aos="fade-up"
+          data-aos-delay="100"
+        >
+          <div class="box">
+            <h3>Site E-Commerce</h3>
             <h4><span>A partir de </span>5990<sup>Dh</sup></h4>
+            <p class="text-justify">
+              Un site e-commerce, est un site web de vente en ligne, ou vous
+              pouvez présenter et vendre vos produits
+            </p>
+            <hr />
+            <ul>
+              <span>
+                <li>SITE RAPIDE</li>
+                <li>MAINTENANCE ET ASSISTANCE</li>
+                <li class="color-orange">LOGO ET DEPLOIEMENT GRATUIT</li>
+                <li class="color-orange">
+                  MINI APPLICATION WEB DE GESTION DE COMMANDE GRATUIT
+                </li>
+                <li class="color-orange">
+                  ADAPTATEE POUR PC, TABLETTE ET TELEPHONE
+                </li>
+              </span>
+            </ul>
             <div class="btn-wrap">
               <a href="#" class="btn-buy">Demander un devis</a>
             </div>
