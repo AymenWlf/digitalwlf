a376c1ade69bcd7b2ce98784b892eb535d78063e
AymenWlf <alinzgohi@gmail.com>
2022-05-02 16:23:37 +0000

    Change ,amespace of mailjet class

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 use DateTime;
-use App\Service\MailJet;
+use App\Class\MailJet;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
