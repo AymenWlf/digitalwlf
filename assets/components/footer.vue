8edc5cd34fbbe232a053ed7b3a08a0f28a01070c
AymenWlf <alinzgohi@gmail.com>
2022-05-04 22:05:59 +0000

    Add social media links

--- assets/components/footer.vue
+++ assets/components/footer.vue
@@ -89,13 +89,28 @@
           >. Tous les droits sont réservés
         </div>
       </div>
-      <!-- <div class="social-links text-center text-lg-right pt-3 pt-lg-0">
-        <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-        <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-        <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-        <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-        <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
-      </div> -->
+      <div class="social-links text-center text-lg-right pt-3 pt-lg-0">
+        <!-- <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a> -->
+        <a
+          target="_blank"
+          href="https://web.facebook.com/digitalwlfdev"
+          class="facebook"
+          ><i class="bx bxl-facebook"></i
+        ></a>
+        <a
+          target="_blank"
+          href="https://www.instagram.com/digital_wlf/"
+          class="instagram"
+          ><i class="bx bxl-instagram"></i
+        ></a>
+        <!-- <a target="_blank" href="#" class="google-plus"><i class="bx bxl-skype"></i></a> -->
+        <a
+          target="_blank"
+          href="https://www.linkedin.com/company/digital-wlf/"
+          class="linkedin"
+          ><i class="bx bxl-linkedin"></i
+        ></a>
+      </div>
     </div>
   </footer>
   <!-- End Footer -->
